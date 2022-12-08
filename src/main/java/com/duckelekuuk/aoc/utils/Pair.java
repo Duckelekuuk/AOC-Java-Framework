44bbe893e77e51252d7bbcb1c102a186545e1b09
Duckelekuuk <duckelekuuk@gmail.com>
2022-12-08 12:36:07 +0100

    WIP

--- src/main/java/com/duckelekuuk/aoc/utils/Pair.java
+++ src/main/java/com/duckelekuuk/aoc/utils/Pair.java
@@ -1,27 +1,25 @@
 package com.duckelekuuk.aoc.utils;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
 
 @Data
 public class Pair<A,B> {
 
-    private A a;
-    private B b;
+    private A left;
+    private B right;
 
-    public Pair(A a, B b) {
-        this.a = a;
-        this.b = b;
+    public Pair(A left, B right) {
+        this.left = left;
+        this.right = right;
     }
 
     public boolean areTheSame() {
-        return a.equals(b);
+        return left.equals(right);
     }
 
     public Map.Entry<A, B> toEntry() {
-        return Map.entry(a, b);
+        return Map.entry(left, right);
     }
 }
